fe7d34204c515445f4e53352e26eec46cd92b31e
Isaac <leechunyin224@gmail.com>
2019-02-22 11:27:57 +0800

    Post: update Post class

--- app/src/main/java/com/edu/codekids/Post.java
+++ app/src/main/java/com/edu/codekids/Post.java
@@ -3,15 +3,23 @@ package com.edu.codekids;
 import java.util.Date;
 
 public class Post {
-    private User PuId, PuName, PuType;
+    private User pUser;
     private String pTitle, pContent;
     private Date pTime;
 
     public Post(){}
 
-    public Post(String title, String content, Date time){
-        pTitle =title; pContent=content; pTime=time;
+    public Post(User user,String title, String content, Date time){
+        pUser = user; pTitle =title; pContent=content; pTime=time;
     }
+
+    public User getUser() { return pUser; }
+
+    public String getUserID () { return pUser.getuId();}
+
+    public String getUserName() { return pUser.getuName();}
+
+    public String getUserType() { return pUser.getuType();}
 
     public Date getpTime() {
         return pTime;
