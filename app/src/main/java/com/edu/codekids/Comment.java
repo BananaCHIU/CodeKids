423180dc0b664f656924d4b469e0aef0e7c20b9a
Isaac <leechunyin224@gmail.com>
2019-02-22 11:22:31 +0800

    Post: update comment class

--- app/src/main/java/com/edu/codekids/Comment.java
+++ app/src/main/java/com/edu/codekids/Comment.java
@@ -3,28 +3,26 @@ package com.edu.codekids;
 import java.util.Date;
 
 public class Comment extends Post {
-    private User CuId, CuName, CuType;
+    private User cUser;
     private String cContent;
     private Date cTime;
     private int cVote;
 
     public Comment(){}
 
-    public Comment(User uid, User name, User type, String content, Date time, int vote){
-        CuId=uid; CuName=name; CuType=type; cContent=content; cTime=time; cVote=vote;
+    public Comment(User user, String content, Date time, int vote){
+        cUser=user; cContent=content; cTime=time; cVote=vote;
     }
 
-    public User getCuId() {
-        return CuId;
+    public String getCuId() {
+        return cUser.getuId();
     }
 
-    public User getCuName() {
-        return CuName;
+    public String getCuName() {
+        return cUser.getuName();
     }
 
-    public User getCuType() {
-        return CuType;
-    }
+    public String getCuType() { return cUser.getuType(); }
 
     public String getcContent() {
         return cContent;
