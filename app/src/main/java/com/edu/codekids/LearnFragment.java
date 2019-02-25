504db8f7d33016e3daf8c30121bb6c4c9e04fad1
ksksks123 <kingstonsin87@gmail.com>
2019-02-25 20:09:52 +0800

    Listview added
    ListView created and is clickable

--- app/src/main/java/com/edu/codekids/LearnFragment.java
+++ app/src/main/java/com/edu/codekids/LearnFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -13,7 +17,34 @@ public class LearnFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_learn, container, false);
+        View view = inflater.inflate(R.layout.fragment_learn, container, false);
+        String[] chapters = {"Chapter 1", "Chapter 2", "Chapter 3" , "Chapter 4" , "Chapter 5"};
+
+        ListView listView = (ListView) view.findViewById(R.id.chapters);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                chapters
+        );
+
+        listView.setAdapter(listViewAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position==0){
+                    Toast.makeText(getActivity(),"Not available now!" , Toast.LENGTH_SHORT).show();
+                } else if (position >0){
+                    Toast.makeText(getActivity(), "Complete the above chapters to unlock!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+        return view;
+
     }
 
 
--- app/src/main/java/com/edu/codekids/QuizFragment.java
+++ app/src/main/java/com/edu/codekids/QuizFragment.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -18,14 +22,30 @@ public class QuizFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
        View view = inflater.inflate(R.layout.fragment_quiz, container, false);
 
-       Button button = (Button) view.findViewById(R.id.button);
-       button.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               Intent intent = new Intent(getActivity(), quiz_start_screenActivity.class);
-                startActivity(intent);
-           }
-       });
+        String[] chapters = {"Chapter 1", "Chapter 2", "Chapter 3" , "Chapter 4" , "Chapter 5"};
+
+        ListView listView = (ListView) view.findViewById(R.id.chapters);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                chapters
+        );
+
+        listView.setAdapter(listViewAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position==0){
+                    Intent intent = new Intent(getActivity(), quiz_start_screenActivity.class);
+                    startActivity(intent);
+                } else if (position >0){
+                    Toast.makeText(getActivity(), "Complete the above quiz to unlock!", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
 
        return view;
 
