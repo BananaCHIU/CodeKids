305c38e6f59470f825b8dd49f30dad77af4c7f75
BananaCHIU <a463028938@gmail.com>
2019-03-06 12:43:14 +0800

    New Post Activity -Working cancel button -Working choice chips -Not Working POST button (Need to upload info to database)
    Forum Fragment
    -Floating action button for new post

--- app/src/main/java/com/edu/codekids/NewPostActivity.java
+++ app/src/main/java/com/edu/codekids/NewPostActivity.java
@@ -2,13 +2,19 @@ package com.edu.codekids;
 
 import android.os.Bundle;
 
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class NewPostActivity extends AppCompatActivity {
 
@@ -16,9 +22,27 @@ public class NewPostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_post);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ImageButton cancel = (ImageButton) findViewById(R.id.button_cancel);
+        ChipGroup chipGroup = (ChipGroup) findViewById(R.id.chip_group);
+        chipGroup.setOnCheckedChangeListener(new ChipGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(ChipGroup chipGroup, int i) {
+
+                Chip chip = chipGroup.findViewById(i);
+                if (chip != null)
+                    Toast.makeText(getApplicationContext(), "Chip is " + chip.getChipText(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 
 }
