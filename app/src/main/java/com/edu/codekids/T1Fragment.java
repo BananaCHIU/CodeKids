72011f82d396e26243f21e536197c1639ce9e4c2
BananaCHIU <a463028938@gmail.com>
2019-03-24 14:52:06 +0800

    changed to fragment

---
+++ app/src/main/java/com/edu/codekids/T1Fragment.java
@@ -0,0 +1,34 @@
+package com.edu.codekids;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class T1Fragment extends Fragment {
+
+
+    public T1Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_t1, container, false);
+        return view;
+    }
+
+}
