15853e121085a01c1432323b724ffa037989c3c4
Isaac <leechunyin224@gmail.com>
2019-04-12 17:19:59 +0800

    added OnClickListener for java learn

--- app/src/main/java/com/edu/codekids/Learn/JavaLearnFragment.java
+++ app/src/main/java/com/edu/codekids/Learn/JavaLearnFragment.java
@@ -28,18 +28,22 @@ public class JavaLearnFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_java_learn, container, false);
-        /*ImageView lock2 = view.findViewById(R.id.lock_2);
-        lock2.bringToFront();
-        ImageView lock3 = view.findViewById(R.id.lock_3);
-        lock3.bringToFront();
-        ImageView lock4 = view.findViewById(R.id.lock_4);
-        lock4.bringToFront();
-        ImageView lock5 = view.findViewById(R.id.lock_5);
-        lock5.bringToFront();
-        ImageView lock6 = view.findViewById(R.id.lock_6);
-        lock6.bringToFront();
-        ImageView lock7 = view.findViewById(R.id.lock_7);
-        lock7.bringToFront();*/
+
+        Button story = view.findViewById(R.id.button_story);
+        story.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button ch1 = view.findViewById(R.id.button_Ch1);
+        story.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         return view;
     }
